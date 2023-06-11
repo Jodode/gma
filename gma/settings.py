c9ac3ff267f9e10827540ce3a1580a85b367e862
Jodode <tilrod.tv@gmail.com>
2023-06-11 16:51:30 +0300

    Try to fix CORS

--- gma/settings.py
+++ gma/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 #}
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +70,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'gma.urls'
 
 TEMPLATES = [
