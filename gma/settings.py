668759fb188b81851ae87f52bae13c620edbcdf8
Jodode <tilrod.tv@gmail.com>
2023-06-14 13:38:22 +0300

    Fix amvera

--- gma/settings.py
+++ gma/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
     'PASSWORD': 'haY80XPHbSMQ',
     'HOST': 'ep-young-firefly-194251.eu-central-1.aws.neon.tech',
     'PORT': '5432',
-    'OPTIONS': {'sslmode': 'require'},
+    'OPTIONS': {'sslmode': 'require', "endpoint": "ep-mute-recipe-123456"},
   }
 }
 
