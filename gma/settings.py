566cd17a6004f94efac2e94b0abc1060e1872f14
Jodode <tilrod.tv@gmail.com>
2023-06-14 13:56:25 +0300

    Fix amvera

--- gma/settings.py
+++ gma/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'gma.wsgi.application'
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'futbik',
     'USER': 'Jodode',
     'PASSWORD': 'haY80XPHbSMQ',
